4bf64789ba6e2b12e97874a67f5a8c0e1b9d76e7
Sam Xifaras <s.xifaras999@gmail.com>
2020-11-03 00:53:08 -0500

    Improved tester function

--- src/agent_tester.py
+++ src/agent_tester.py
@@ -5,17 +5,23 @@ Defines functions for testing and generating reports on agent performance
 from src.agents.agents import RandomAgent
 from src.game.gamestate import GameStateImpl
 
+import time
 import src.game.constants as c
+
+import pickle
 
 def run_test(num_trials=100, with_ui=False, agent=RandomAgent):
     infos = []
     for i in range(num_trials):
         print('running trial', i)
+        st = time.time()
         gs = GameStateImpl()
         turn = 0
         while gs.state() != 'lose':
             if turn % 100 == 0:
                 print('turn', turn)
+            if turn > 200:
+                break
             act = agent.decide(gs)
             moved = gs.execute_action(act, c.PLAYER)
             if moved:
@@ -23,6 +29,14 @@ def run_test(num_trials=100, with_ui=False, agent=RandomAgent):
 
             turn += 1
 
+        et = time.time()
+
         highest_tile = max([max(row) for row in gs.matrix])
-        infos.append((gs.score(), turn, highest_tile, gs.matrix))
-        print('trial complete. score:', gs.score(), '; highest tile:', highest_tile)
+        info_tuple = (i, et - st, gs.get_score(), turn, highest_tile, gs.matrix)
+        infos.append(info_tuple)
+        print('trial {} complete. time elapsed: {:0.2f}s, score: {}, highest tile: {}, num of turns: {}\nfinal matrix: {}'
+              .format(*info_tuple))
+        break
+
+    with open('out.pkl', 'wb+') as f:
+        pickle.dump(infos, f)
