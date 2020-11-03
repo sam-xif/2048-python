a1798c62a781acd35b53b0ff79475ba50dd77643
Sam Xifaras <s.xifaras999@gmail.com>
2020-11-02 21:50:21 -0500

    Changed evaluation function and made VariableDepthExpectimaxinherit DepthLimitedExpectimax

--- src/agents/agents.py
+++ src/agents/agents.py
@@ -40,12 +40,12 @@ class DepthLimitedExpectimax(Base2048Agent):
         """
         Implement depth-limited expectimax
         """
-        mx = self._max_decision(game_state, depth=3)
+        mx = self._max_decision(game_state, depth=2)
         return mx
 
     def evaluate(self, game_state):
         mat = game_state.matrix
-        return game_state.get_score() + np.sum([np.sum([x for x in row]) for row in mat])
+        return game_state.get_score() + np.sum([np.sum([x**2 for x in row]) for row in mat])
 
     def _max_decision(self, game_state: BaseGameState, depth=4):
         if depth < 0:
@@ -80,7 +80,7 @@ class DepthLimitedExpectimax(Base2048Agent):
         return np.max([self._expectation_value(st, depth) for st in successor_states])
 
 
-class VariableDepthExpectimax(Base2048Agent):
+class VariableDepthExpectimax(DepthLimitedExpectimax):
     def __init__(self):
         super().__init__()
 
@@ -105,39 +105,3 @@ class VariableDepthExpectimax(Base2048Agent):
 
         mx = self._max_decision(game_state, depth=depth)
         return mx
-
-    def evaluate(self, game_state):
-        mat = game_state.matrix
-        return game_state.get_score() + np.sum([np.sum([x for x in row]) for row in mat])
-
-    def _max_decision(self, game_state: BaseGameState, depth=4):
-        if depth < 0:
-            raise ValueError('Depth cannot be less than 0')
-
-        if depth == 0:
-            return self.evaluate(game_state)
-
-        # max over all player actions
-        actions = game_state.get_allowed_actions(c.PLAYER)
-        successor_states = [game_state.get_successor(act, c.PLAYER) for act in actions]
-        arr = [self._expectation_value(st, depth) for st in successor_states]
-        max_ = np.max(arr)
-        indices = [i for i, x in enumerate(arr) if abs(x - max_) <= 0.001]
-        return actions[random.choice(indices)]  # Randomly break ties
-
-    def _expectation_value(self, game_state: BaseGameState, depth):
-        # branch into all possible placements of 2-tile
-        successor_states = game_state.get_successors(c.ADVERSARY)
-        if len(successor_states) == 0:
-            return self.evaluate(game_state)
-        return np.average([self._max_value(st, depth - 1) for st in successor_states])
-
-    def _max_value(self, game_state: BaseGameState, depth):
-        if depth == 0:
-            return self.evaluate(game_state)
-
-        successor_states = game_state.get_successors(c.PLAYER)
-        if len(successor_states) == 0:
-            return self.evaluate(game_state)
-
-        return np.max([self._expectation_value(st, depth) for st in successor_states])
